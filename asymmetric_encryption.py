cefb89abb36e1e87a87f72d0b62f24ac4d539df4
Drew Siedel <dts.siedel@gmail.com>
2016-10-27 18:06:07 -0400

    Added beginnings of key generation

--- asymmetric_encryption.py
+++ asymmetric_encryption.py
@@ -1,4 +1,10 @@
 import math
+import random
+
+#gets a coprime of n. A coprime is a value x such that the
+#greatest common denominator between n and x is 1
+def get_coprime(n):
+    pass
 
 #tells if an int n is prime
 def is_prime(n):
@@ -8,16 +14,21 @@ def is_prime(n):
 
 #gets a random prime from the list of primes
 def get_random_prime(primes):
-    #generate random number in range
-    #get number at that index and return it
-    pass
+    return random.choice(primes)
 
 #generates a public and private key, storing them in a file
+#note that the primes generated in real RSA encryption are
+#1024 bits long, to foil any reasonable attempt to factor them
 def generate_key():
-    primes = [x for x in range(0,1000000) if is_prime(x)]
-    print primes
-    num1 = get_random_prime(primes)
-    num2 = get_random_prime(primes)
+    primes = [x for x in range(2,1000000) if is_prime(x)]
+    prime1 = get_random_prime(primes)
+    prime2 = get_random_prime(primes)
+    n = prime1 * prime2
+    totient = (prime1 - 1) * (prime2 - 1) #phi, in the docs
+    e = get_coprime(totient)
 
+
+    public_key = str(n) + "-" + str(e)
+    #private_key = ...
 
 generate_key()
